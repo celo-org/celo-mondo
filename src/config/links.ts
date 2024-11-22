9fda73833793c867d06fff42e5e49cdd86eabb1e
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-11-22 14:56:55 +0100

    remove unused links

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,16 +2,13 @@ import { config } from 'src/config/config';
 
 export const links = {
   home: 'https://mondo.celo.org',
-  celo: 'https://celo.org',
   discord: 'https://discord.gg/celo',
   github: 'https://github.com/celo-org/celo-mondo',
   twitter: 'https://twitter.com/celo',
   docs: 'https://docs.celo.org',
   forum: 'https://forum.celo.org',
   governance: 'https://github.com/celo-org/governance',
-  delegate: 'https://github.com/celo-org/celo-mondo/blob/main/src/config/delegates.json',
   // Explorers
-  blockscout: 'https://explorer.celo.org',
   celoscan: config.isAlfajores ? 'https://alfajores.celoscan.io/' : 'https://celoscan.io',
   celoscanApi: config.isAlfajores
     ? 'https://api-alfajores.celoscan.io/api'
