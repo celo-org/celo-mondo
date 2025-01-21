0e6e3efae65d0bdf80eb4dd7753bd2ce93df52a5
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-01-21 15:43:56 +0100

    dynamc og title for staking

--- src/app/staking/[address]/page.tsx
+++ src/app/staking/[address]/page.tsx
@@ -3,6 +3,15 @@ import ValidatorPage from 'src/features/staking/page';
 type Params = { params: { address: Address } };
 export const dynamicParams = true;
 
+export async function generateMetadata({ params: { address } }: Params) {
+  return {
+    openGraph: {
+      title: `${address}`,
+      description: `Stake with ${address}`,
+    },
+  };
+}
+
 export default function Page({ params: { address } }: Params) {
   return <ValidatorPage address={address} />;
 }
