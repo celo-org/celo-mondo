1c26f0e34fbd461f39bdde8068a85e9ba6710a7b
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-02-25 17:13:44 +0100

    prettier

--- src/test/anvil/utils.tsx
+++ src/test/anvil/utils.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { renderHook as _renderHook } from '@testing-library/react';
-import React from 'react';
 import { PublicClient, createPublicClient, createTestClient, createWalletClient, http } from 'viem';
 import { mnemonicToAccount } from 'viem/accounts';
 import { celo } from 'viem/chains';
