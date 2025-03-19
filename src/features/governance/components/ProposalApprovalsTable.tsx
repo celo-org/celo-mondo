e968ab35d7e261525c8712e65e5586238cd1b3e7
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-03-19 19:26:11 +0100

    if a proposal is in referendum or execution fetch and show the approvers who confirmed it.
    also add ErrorBoundaryInline so that the entire page doesnt crash just the component with the problems

---
+++ src/features/governance/components/ProposalApprovalsTable.tsx
@@ -0,0 +1,69 @@
+import { SpinnerWithLabel } from 'src/components/animation/Spinner';
+import { Identicon } from 'src/components/icons/Identicon';
+import { Collapse } from 'src/components/menus/Collapse';
+import { ShortAddress } from 'src/components/text/ShortAddress';
+import { useProposalApprovals } from 'src/features/governance/hooks/useProposalApprovals';
+import { Address } from 'viem';
+
+export function ProposalApprovalsTable({ proposalId }: { proposalId: number }) {
+  const { isLoading, confirmations, required } = useProposalApprovals(proposalId);
+  const fraction = isLoading ? '? / 3' : `${confirmations?.length || 0} / ${required}`;
+  return (
+    <div className="hidden md:block">
+      <Collapse
+        button={
+          <h2 className="text-left font-serif text-2xl">
+            Approved By
+            <span className="pl-4 text-sm text-taupe-600">{fraction}</span>
+          </h2>
+        }
+        buttonClasses="w-full"
+        defaultOpen={true}
+      >
+        <ConfirmationsTable isLoading={isLoading} confirmations={confirmations} />
+      </Collapse>
+    </div>
+  );
+}
+
+// currently actually just the addresses that have confirmed the transaction
+function ConfirmationsTable({
+  isLoading,
+  confirmations,
+}: {
+  isLoading: boolean;
+  confirmations: readonly Address[] | undefined;
+}) {
+  if (isLoading) {
+    return (
+      <SpinnerWithLabel size="md" className="py-6">
+        Loading confirmations
+      </SpinnerWithLabel>
+    );
+  }
+
+  if (!confirmations?.length) {
+    return (
+      <div className="py-6 text-center text-sm text-gray-600">
+        No approvers have confirmed this proposal yet
+      </div>
+    );
+  }
+
+  return (
+    <table>
+      <tbody>
+        {confirmations.map((approver) => (
+          <tr key={approver}>
+            <td className="py-2">
+              <Identicon address={approver} size={20} />
+            </td>
+            <td className="px-4 py-2 text-sm">
+              <ShortAddress address={approver} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
---
+++ src/features/governance/hooks/useProposalApprovals.ts
@@ -0,0 +1,125 @@
+import { governanceABI, multiSigABI } from '@celo/abis-12';
+import { StaleTime } from 'src/config/consts';
+import { Addresses } from 'src/config/contracts';
+import { useProposalDequeueIndex } from 'src/features/governance/hooks/useProposalQueue';
+import { encodeFunctionData } from 'viem';
+import { useReadContract, useReadContracts } from 'wagmi';
+
+export function useProposalApprovals(proposalId: number) {
+  // get the address of the approver contract (a multisig contract)
+  const approverAddress = useReadContract({
+    address: Addresses.Governance,
+    abi: governanceABI,
+    functionName: 'approver',
+    args: [],
+    query: {
+      enabled: proposalId !== undefined,
+      staleTime: StaleTime.Long,
+    },
+  });
+  const { index, isLoading: isIndexLoading } = useProposalDequeueIndex(proposalId);
+  // build an approval txn which is needed to find the number above approvals
+
+  const approveTxData =
+    index &&
+    index !== -1n &&
+    encodeFunctionData({
+      abi: governanceABI,
+      functionName: 'approve',
+      args: [BigInt(proposalId), index],
+    });
+
+  // getConfirmations might be more accuratly get addresses of owners who have approved
+  const numberOfTxInMultiSig = useReadContract({
+    address: approverAddress.data,
+    abi: multiSigABI,
+    functionName: 'getTransactionCount',
+    // pending, executed
+    args: [true, true], //todo need to find the correct index which is different from the dequeue index
+    query: {
+      enabled: approverAddress.isSuccess,
+      staleTime: StaleTime.Long,
+    },
+  });
+
+  const allTransactionsInMultisig = useReadContracts({
+    contracts: Array(Number(numberOfTxInMultiSig.data ?? 0))
+      .fill(0)
+      .map((_, i) => ({
+        address: approverAddress.data,
+        abi: multiSigABI,
+        functionName: 'transactions',
+        args: [i], //todo need to find the correct index which is different from the dequeue index
+      })),
+    query: {
+      enabled: approverAddress.isSuccess && numberOfTxInMultiSig.isSuccess,
+      staleTime: StaleTime.Long,
+    },
+  });
+  const indexOfTXToFindApproversOF = allTransactionsInMultisig.data?.findIndex((query) => {
+    if (query.status === 'success' && query.result) {
+      const [_destination, _value, data, _isExecuted] = query.result as [
+        string,
+        bigint,
+        string,
+        boolean,
+      ];
+      return data === approveTxData;
+    }
+  });
+
+  const confirmations = useReadContract({
+    address: approverAddress.data,
+    abi: multiSigABI,
+    functionName: 'getConfirmations',
+    args: [indexOfTXToFindApproversOF !== -1 ? BigInt(indexOfTXToFindApproversOF ?? 0) : 0n], //todo need to find the correct index which is different from the dequeue index
+    query: {
+      enabled:
+        approverAddress.isSuccess &&
+        indexOfTXToFindApproversOF !== undefined &&
+        indexOfTXToFindApproversOF !== -1,
+      staleTime: StaleTime.Long,
+    },
+  });
+
+  const required = useReadContract({
+    address: approverAddress.data,
+    abi: multiSigABI,
+    functionName: 'required',
+    args: [],
+    query: {
+      enabled: approverAddress.isSuccess,
+      staleTime: StaleTime.Long,
+    },
+  });
+
+  const owners = useReadContract({
+    address: approverAddress.data,
+    abi: multiSigABI,
+    functionName: 'getOwners',
+    // pending, executed
+    args: [], //todo need to find the correct index which is different from the dequeue index
+    query: {
+      enabled: approverAddress.isSuccess,
+      staleTime: StaleTime.Long,
+    },
+  });
+  // getRequired
+  return {
+    isLoading:
+      confirmations.isLoading ||
+      isIndexLoading ||
+      approverAddress.isLoading ||
+      owners.isLoading ||
+      allTransactionsInMultisig.isLoading,
+    isError:
+      confirmations.isError ||
+      isIndexLoading ||
+      approverAddress.isError ||
+      allTransactionsInMultisig.isError,
+    confirmations: confirmations.data,
+    transactions: allTransactionsInMultisig.data,
+    required: required.data,
+    owners: owners.data,
+  };
+}
