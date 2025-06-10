e810fd306eb79f36245048c3ef1482367bbcc59d
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-06-10 09:56:57 +0200

    bridge descriptions

--- src/app/bridge/page.tsx
+++ src/app/bridge/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { A_Blank } from 'src/components/buttons/A_Blank';
 import { SolidButton } from 'src/components/buttons/SolidButton';
 import { ChevronIcon } from 'src/components/icons/Chevron';
+import { HelpIcon } from 'src/components/icons/HelpIcon';
 import { Section } from 'src/components/layout/Section';
 import { H1 } from 'src/components/text/headers';
 import { config } from 'src/config/config';
@@ -15,7 +16,7 @@ interface Bridge {
   operator: string;
   href: string;
   logo: any;
-  cel2Only?: boolean;
+  description: string;
 }
 
 const BRIDGES: Bridge[] = [
@@ -24,18 +25,22 @@ const BRIDGES: Bridge[] = [
     operator: 'Superbridge',
     href: `https://superbridge.app/celo${config.chain.testnet ? '-testnet' : ''}`,
     logo: '/logos/superbridge.jpg',
+    description: 'Native Celo L2 bridge. Good for moving CELO and ETH between Ethereum and Celo.',
   },
   {
     name: 'Squid Router',
     operator: 'Squid',
     href: 'https://v2.app.squidrouter.com',
     logo: SquidLogo,
+    description:
+      'Axelar based cross chain DEX. Good for moving stablecoins between chains, or swapping directly between assets.',
   },
   {
     name: 'Portal Bridge',
     operator: 'Wormhole',
     href: 'https://portalbridge.com',
     logo: PortalLogo,
+    description: 'Wormhole based bridge. Good for wormhole assets on Celo.',
   },
 ];
 
@@ -55,14 +60,17 @@ export default function Page() {
   );
 }
 
-function BridgeLink({ name, operator, href, logo }: Bridge) {
+function BridgeLink({ name, operator, href, logo, description }: Bridge) {
   return (
     <div className="flex items-center justify-between border border-taupe-300 bg-white p-4 sm:gap-32 sm:p-5">
       <div className="flex items-center space-x-4">
         <Image src={logo} width={60} height={60} alt="" className="rounded-full" />
         <div className="flex flex-col">
           <h2 className="font-serif text-xl">{name}</h2>
-          <h3 className="text-sm">{`By ${operator}`}</h3>
+          <div className="flex flex-row gap-2">
+            <h3 className="text-sm">{`By ${operator}`}</h3>
+            <HelpIcon text={description} type="tooltip" />
+          </div>
         </div>
       </div>
       <SolidButton className="all:p-0">
