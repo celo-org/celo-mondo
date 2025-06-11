e954606346fc11a34bbfef3f96d1f7be58dc0d20
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-06-11 16:12:53 +0200

    Update route.test.ts

--- src/app/delegate/api/register/route.test.ts
+++ src/app/delegate/api/register/route.test.ts
@@ -31,7 +31,7 @@ const getValidFormData = async () => {
   return data;
 };
 
-it('successfuly calls PR creation', async () => {
+it('successfully calls PR creation', async () => {
   const createDelegationPRMock = vi.spyOn(utils, 'createDelegationPR');
   const isAddressAnAccountMock = vi.spyOn(utils, 'isAddressAnAccount');
 
