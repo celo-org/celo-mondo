15c441fa317c95a07687fcf64f19025c38c0981a
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-06-19 11:13:45 +0200

    refactor

--- src/app/api/governance/events/route.ts
+++ src/app/api/governance/events/route.ts
@@ -1,39 +1,42 @@
-import { sql } from 'drizzle-orm';
+import { and, eq, sql } from 'drizzle-orm';
+import { type NextRequest } from 'next/server';
 import 'server-only';
 import database from 'src/config/database';
 import { eventsTable } from 'src/db/schema';
 
-export async function GET(request: Request): Promise<Response> {
+export async function GET(request: NextRequest): Promise<Response> {
   const { searchParams } = new URL(request.url);
 
   if (!searchParams.has('chainId')) {
-    return new Response(JSON.stringify({ error: 'Missing chainId' }), {
-      status: 400,
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return Response.json(
+      { error: 'Missing chainId' },
+      {
+        status: 400,
+      },
+    );
   }
   if (!searchParams.has('eventName')) {
-    return new Response(JSON.stringify({ error: 'Missing eventName' }), {
-      status: 400,
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return Response.json(
+      { error: 'Missing eventName' },
+      {
+        status: 400,
+      },
+    );
   }
 
-  const query = sql`
-    ${eventsTable.eventName} = ${searchParams.get('eventName')}
-    AND ${eventsTable.chainId} = ${parseInt(searchParams.get('chainId')!, 10)}
-  `;
-
+  const filters = [
+    eq(eventsTable.eventName, searchParams.get('eventName')!),
+    eq(eventsTable.chainId, parseInt(searchParams.get('chainId')!, 10)),
+  ];
   if (searchParams.has('proposalId')) {
-    query.append(sql`
-      AND ${eventsTable.topics}[2] = ${searchParams.get('proposalId')}
-    `);
+    filters.push(eq(sql`${eventsTable.topics}[2]`, searchParams.get('proposalId')!));
   }
 
-  const events = await database.select().from(eventsTable).where(query).limit(1000);
+  const events = await database
+    .select()
+    .from(eventsTable)
+    .where(and(...filters))
+    .limit(1000);
 
-  return new Response(JSON.stringify(events), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  });
+  return Response.json(events);
 }
