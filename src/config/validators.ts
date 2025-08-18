7b75d3cfe1426c5dc5c1797677a211cce4e80daa
Clemens Otto <id@clemens.fi>
2025-08-18 15:25:58 +0200

    Update delegate/validator profile information (clemens.eth / atweb3) (#276)
    * update delegate profile and validator URL
    
    * update delegate and validator profile #2

--- src/config/validators.ts
+++ src/config/validators.ts
@@ -346,7 +346,8 @@ export const VALIDATOR_GROUPS: Record<string, ValidatorInfo> = {
   '0xb434FeB47D6154B4B4058DF5C9fCeD123dB9aBF6': {
     logo: '/logos/validators/atweb3.jpg',
     links: {
-      website: 'https://www.atweb3.io',
+      website: 'https://atweb3.co',
+      twitter: 'https://x.com/atweb3_gmbh',
     },
   },
   '0xD19FB36B7F433fe13820767ef6d0E26FDbaB68CC': {
