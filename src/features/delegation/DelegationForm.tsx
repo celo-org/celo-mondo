0679ec6c3d5f892f3e440dcbafd958d067cfa1e9
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-11-06 11:33:10 +0100

    fix

--- src/features/delegation/DelegationForm.tsx
+++ src/features/delegation/DelegationForm.tsx
@@ -129,7 +129,10 @@ export function DelegationForm({
                   id="default-checkbox"
                   type="checkbox"
                   value={values.customDelegatee ? 'true' : ''}
-                  onChange={() => setFieldValue('customDelegatee', !values.customDelegatee)}
+                  onChange={() => {
+                    setFieldValue('delegatee', '' as Address);
+                    setFieldValue('customDelegatee', !values.customDelegatee);
+                  }}
                   className="h-4 w-4 rounded-sm border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
                 />
                 <label
