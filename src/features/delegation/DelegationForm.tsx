5a996abd166c4d4772635e2065ff8047719f6225
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2025-11-06 11:21:21 +0100

    fix

--- src/features/delegation/DelegationForm.tsx
+++ src/features/delegation/DelegationForm.tsx
@@ -259,6 +259,7 @@ function DelegateeField({
           className="btn btn-outline w-full border-taupe-300 px-3 text-left disabled:input-disabled hover:border-taupe-300 hover:bg-white hover:text-black"
           value={field.value}
           onPaste={(evt) => {
+            evt.preventDefault();
             helpers.setValue(ensure0x(evt.clipboardData.getData('text')), false);
           }}
           onChange={(evt) => {
