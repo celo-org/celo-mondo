e3cea4715a14c7310c7cfdf8dbb9c2b542e2b9fc
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-12-22 00:41:49 -0600

    fix bug in proposals update script (#420)
    should set not compare

--- src/scripts/updateProposalStages.ts
+++ src/scripts/updateProposalStages.ts
@@ -93,7 +93,7 @@ async function updateProposalStages() {
           proposal.stage === ProposalStage.Execution &&
           proposal.transactionCount === 0
         ) {
-          onChainStage === ProposalStage.Adopted;
+          onChainStage = ProposalStage.Adopted;
         }
 
         updates.push({
