8db948326bd863f703e6224ee9f6c57681159965
Pavel Hornak <pavel.hornak@clabs.co>
2026-04-07 15:30:56 +0200

    fix: initialize useIsMiniPay synchronously to avoid flash of non-MiniPay UI

--- src/utils/useIsMiniPay.ts
+++ src/utils/useIsMiniPay.ts
@@ -1,12 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export function useIsMiniPay() {
-  const [isMiniPay, setIsMiniPay] = useState(false);
-
-  useEffect(() => {
-    // @ts-ignore
-    setIsMiniPay(typeof window !== 'undefined' && !!window.ethereum?.isMiniPay);
-  }, []);
-
+  // @ts-ignore
+  const [isMiniPay] = useState(() => typeof window !== 'undefined' && !!window.ethereum?.isMiniPay);
   return isMiniPay;
 }
