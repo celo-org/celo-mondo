c5af84a2decbc44d1fd297448d846543c012a6bd
Leszek Stachowski <leszek.stachowski@self.xyz>
2026-04-30 14:22:15 +0200

    fix: indexedDB is not defined

--- src/config/wagmi.tsx
+++ src/config/wagmi.tsx
@@ -11,44 +11,49 @@ import {
   walletConnectWallet,
 } from '@rainbow-me/rainbowkit/wallets';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useMemo } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { config, infuraRpcUrl } from 'src/config/config';
 import { Color } from 'src/styles/Color';
 import { celo, celoAlfajores } from 'viem/chains';
 import { WagmiProvider, createConfig, fallback, http } from 'wagmi';
 
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: 'Recommended for Celo',
-      wallets: [
-        valoraWallet,
-        metaMaskWallet,
-        rainbowWallet,
-        trustWallet,
-        braveWallet,
-        safeWallet,
-        walletConnectWallet,
-        injectedWallet,
-      ],
-    },
-  ],
-  { appName: config.appName, projectId: config.walletConnectProjectId },
-);
+function buildWagmiConfig() {
+  const connectors = connectorsForWallets(
+    [
+      {
+        groupName: 'Recommended for Celo',
+        wallets: [
+          valoraWallet,
+          metaMaskWallet,
+          rainbowWallet,
+          trustWallet,
+          braveWallet,
+          safeWallet,
+          walletConnectWallet,
+          injectedWallet,
+        ],
+      },
+    ],
+    { appName: config.appName, projectId: config.walletConnectProjectId },
+  );
 
-export const wagmiConfig = createConfig({
-  chains: [config.chain],
-  connectors,
-  syncConnectedChain: false, // only have 1 chain per deployment
-  transports: {
-    [celo.id]: fallback([http(config.chain.rpcUrls.default.http[0]), http(infuraRpcUrl)]),
-    [celoAlfajores.id]: http(config.chain.rpcUrls.default.http[0]),
-  },
-});
+  return createConfig({
+    chains: [config.chain],
+    connectors,
+    syncConnectedChain: false, // only have 1 chain per deployment
+    transports: {
+      [celo.id]: fallback([http(config.chain.rpcUrls.default.http[0]), http(infuraRpcUrl)]),
+      [celoAlfajores.id]: http(config.chain.rpcUrls.default.http[0]),
+    },
+  });
+}
 
 const queryClient = new QueryClient();
 
 export function WagmiContext({ children }: { children: React.ReactNode }) {
+  const wagmiConfig = useMemo(() => buildWagmiConfig(), []);
+
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
